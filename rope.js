427754c70b2408134ae2dc3cd342ff1015ce7f88
Danilo <dnlmcrd@gmail.com>
2019-09-18 14:58:56 -0300

    Some changes

--- rope.js
+++ rope.js
@@ -1,5 +1,10 @@
+//############################
+//Processing Rope
+//############################
+
+//Classes -------------
 class Knot {
-	constructor() {
+	constructor(x, y, d = 20) {
 		this.x = x;
 		this.y = y;
 		this.vx = 0;
@@ -10,24 +15,40 @@ class Knot {
 }
 
 class Rope {
-	constructor() {
+	constructor(x, y, knots, size = 20, angle = 180) {
 		this.knots = [];
 		
 	}
 	
 }
 
+//Globals -------------
+g = [];
+g.p = 0;
 
+//Setup ---------------
 function setup() {
 	resizeCanvas(640,480,1);
-	background(100);
+	noStroke();
 	
+	a = new Knot(10, 10);
+	b = new Knot(10, 10, 30);
+	
+	print(a);
+	print(b);
 }
 
-var i = 0;
-
+//Draw ----------------
 function draw() {
 	background(100);
-	i+= 0.1;
-	rect(20*i, i*i, 20, 20); 
+	
+	g.p += 0.1;
+	
+	fill(204, 102, 0);
+	noStroke();
+	rect(20*g.p, g.p*g.p, 20, 20); 
+	
+	noFill();
+	stroke(0, 0, 0);
+	bezier(85, 20, 10*sin(g.p), 10, 90, 90, 15, 80);
 }
