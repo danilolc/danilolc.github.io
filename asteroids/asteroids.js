11e27f48eb27fda492fe04f294f7009a17d995ea
Danilo <dnlmcrd@gmail.com>
2019-10-31 12:03:01 -0300

    Add asteroids

---
+++ asteroids/asteroids.js
@@ -0,0 +1,21 @@
+//############################
+//Processing Asteroids
+//############################
+
+//Classes -------------
+
+
+//Globals -------------
+var Sx = 640;
+var Sy = 480;
+
+//Setup ---------------
+function setup() {
+	resizeCanvas(Sx,Sy,1);
+	background(200);
+}
+
+//Draw ----------------
+function draw() {
+	
+}
