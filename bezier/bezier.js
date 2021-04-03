c39cd36b028419547414452678e9b1a1478dba01
Danilo <dnlmcrd@gmail.com>
2021-04-03 16:05:45 -0300

    Bezier

---
+++ bezier/bezier.js
@@ -0,0 +1,24 @@
+//############################
+//Processing Bezier
+//############################
+
+function setup() {
+
+    var canvas = createCanvas(640, 480);
+    canvas.parent('canvasForHTML');
+    
+    //setColor(0);
+    line(0, 0, 640, 480);
+    line(0, 480, 640, 0);
+
+}
+
+function draw() {
+	
+
+}
+
+function mousePressed() {
+
+
+}
