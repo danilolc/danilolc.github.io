635243c948b9eb67c119aa717b7d62c12289cbf8
Danilo <dnlmcrd@gmail.com>
2021-04-08 22:57:49 -0300

    Some changes

--- bezier/bezier.js
+++ bezier/bezier.js
@@ -2,25 +2,44 @@
 //Processing Bezier
 //############################
 
-
 const ScreenX = 640;
 const ScreenY = 480;
+
+const LISTA_FOCOS_INICIAL = [
+
+    [50, 202],
+    [73, 341],
+    [242, 451],
+    [553, 357],
+    [556, 119],
+    [242, 51],
+
+];
+
+var lista_focos = LISTA_FOCOS_INICIAL.slice()
 
 var drag = false;
 var selected = 1;
 
 var desenha_hull = false;
+var casteljau_point = false;
+var casteljau_t = 0.5;
 
 var atualiza_tela = true;
 
 var subdivisoes = 100;
+var epsilon = 1;
 
+var draw_2 = false;
+
+// Operações com pontos e vetores
 const add = (p1, p2) => [p1[0] + p2[0], p1[1] + p2[1]]
 const sub = (p1, p2) => [p1[0] - p2[0], p1[1] - p2[1]]
 const mult = (k, p) => [k*p[0], k*p[1]]
 const vecp = (v1, v2) => v1[0]*v2[1] - v1[1]*v2[0]
 const dotp = (v1, v2) => v1[0]*v2[0] + v1[1]*v2[1]
 
+// a! / b!
 function product_Range(a, b) {
 
   var prd = a;
@@ -34,6 +53,7 @@ function product_Range(a, b) {
 
 }
 
+// Combinações n escolhe r
 function comb(n, r) {
 
     if (n < r)
@@ -116,20 +136,10 @@ function convex_hull(_points) {
     U.pop();
 
     return L.concat(U);
+
 }
 
-
-var lista_focos = [
-
-    [50, 202],
-    [73, 341],
-    [242, 451],
-    [553, 357],
-    [556, 119],
-    [242, 51],
-
-];
-
+// Exercise 6.21 Degree raising
 function acrescentar_ponto() {
 
     var n = lista_focos.length - 1
@@ -148,6 +158,37 @@ function acrescentar_ponto() {
 
 }
 
+function casteljau(lista, t, bleft, bright) { // Função recursiva que diminui o tamanho da lista até sobrar um ponto
+
+    const len = lista.length;
+
+    //Vai salvando os pontos de Bleft e Bright em seus vetores
+    if (bleft != undefined)
+        bleft.push(lista[0])
+    if (bright != undefined)
+        bright.push(lista[len-1])
+    
+    if (len == 1)
+        return lista[0]
+
+    var lista2 = []
+    for (var i = 0; i < len - 1; i++)
+        lista2.push(add(mult(1-t, lista[i]), mult(t, lista[i+1])))
+
+    return casteljau(lista2, t, bleft, bright)
+
+}
+
+function corta_em_t() {
+
+    var bleft = []
+    casteljau(lista_focos, casteljau_t, bleft)
+
+    lista_focos = bleft
+    atualiza_tela = true;
+
+}
+
 
 function draw_square(x, y, rad) {
 
@@ -157,7 +198,7 @@ function draw_square(x, y, rad) {
 
 }
 
-function desenha_focos(p, i) {
+function desenha_foco(p, i) {
 
     strokeWeight(1.5);
     
@@ -170,19 +211,16 @@ function desenha_focos(p, i) {
 
 }
 
-function desenha_curva(resolucao) {
-
-    strokeWeight(1);
-    stroke(color(0,0,0));
+function desenha_curva1(lista, resolucao) {
 
     const f = (t) => { // Define o f(t) da curva
 
         var x = 0;
         var y = 0;
-        var n = lista_focos.length - 1
+        var n = lista.length - 1
         var SB = 0;
 
-        lista_focos.map((p, i) => {
+        lista.map((p, i) => {
 
             if (i == 0) var B = pow(1-t, n) // Teve que separar os casos por causa do problema de 0^0
             else if (i == n) var B = pow(t, n)
@@ -205,7 +243,7 @@ function desenha_curva(resolucao) {
 
     var t = dist;
 
-    var p0 = lista_focos[0]
+    var p0 = lista[0]
 
     do { // Desenha linhas de p0 até p1
 
@@ -221,6 +259,103 @@ function desenha_curva(resolucao) {
 }
 
 
+/*function quase_reta_fast(lista, eps) {
+
+    // ||u - prog(v, u)|| < eps  <==> dot(v,v)*dot(u,u) - dot(u,v)^2 < eps*dot(v,v)
+
+    var len = lista.length
+    if (len <= 2)
+        return true;
+
+    var B = lista[0]
+    var A = lista[len - 1]
+
+    // Pre calcula os valores comuns
+    var v = sub(A, B)
+    var vdv = dotp(v,v)
+    if (vdv == 0)
+        return false
+    var epv = eps*vdv
+
+    for (var i = 1; i < len - 1; i++) {
+        var u = sub(lista[i], B)
+        var udu = dotp(u,u)
+        var udv = dotp(u,v)
+        if (vdv*udu - udv*udv > epv)
+            return false; //Não é reta
+    }
+
+    return true
+
+}*/
+
+
+function quase_reta(lista, eps) {
+
+    // Retorna falso se algum ponto tem dist^2 maior que eps da reta
+
+    var len = lista.length
+    if (len <= 2)
+        return true;
+
+    var B = lista[0]
+    var A = lista[len - 1]
+
+    if (A[0] == B[0] && A[1] == B[1])
+        return false
+
+    // Testa para cada ponto no interior da lista
+    for (var i = 1; i < len - 1; i++) {
+        var P = lista[i]
+
+        var u = sub(P, B)
+        var v = sub(A, B)
+
+        // Calcula o tamanho da projeção de u em v
+        var proj = mult( dotp(u,v)/dotp(v,v), v )
+        var perp = sub( u, proj )
+
+        if (dotp(perp, perp) > eps)
+            return false; //Não é reta
+
+    }
+
+    return true
+
+}
+
+// Desenha linha ligando os pontos da lista
+function draw_control_polygon(lista) {    
+
+    var len = lista.length
+    if(len <= 1) return 
+
+    for (var i = 1; i < len; i++) {
+
+        var p0 = lista[i-1]
+        var p1 = lista[i]
+        line(p0[0], p0[1], p1[0], p1[1]);
+
+    }
+}
+
+// Função recursiva que desenha a curva com o método descrito sessão 6.10.1
+function desenha_curva2(lista, eps) {
+
+    if (quase_reta(lista, eps))
+        return draw_control_polygon(lista)
+
+    var bleft = []
+    var bright = []
+    casteljau(lista, 0.5, bleft, bright)
+
+    desenha_curva2(bleft, eps)
+    desenha_curva2(bright, eps)
+    
+    atualiza_tela = true;
+
+}
+
 function setup() {
 
     var canvas = createCanvas(ScreenX, ScreenY);
@@ -228,6 +363,7 @@ function setup() {
 
 }
 
+// Main Loop
 function draw() {
 	
     if (atualiza_tela) {
@@ -247,9 +383,34 @@ function draw() {
 
         }
 
+        strokeWeight(1.5);
+        if(draw_2) {
 
-        desenha_curva(subdivisoes)
-        lista_focos.map(desenha_focos)
+            if (quase_reta(lista_focos, epsilon))
+                stroke(color(100,200,100));
+            else
+                stroke(color(255,200,100));
+            
+            desenha_curva2(lista_focos, epsilon)
+
+        } else {
+
+            stroke(color(255,100,255));
+            
+            desenha_curva1(lista_focos, subdivisoes)
+
+        }
+
+        lista_focos.map(desenha_foco)
+
+        if (casteljau_point) {
+            var point = casteljau(lista_focos, casteljau_t);
+
+            strokeWeight(1);
+            stroke(color(0,0,150));
+            draw_square(point[0], point[1], 3)
+
+        }
 
 
         atualiza_tela = false;
@@ -273,11 +434,11 @@ function deletar_ponto() {
 
 }
 
-function keyPressed() {
+/*function keyPressed() {
 
     if (keyCode === DELETE) deletar_ponto()
 
-}
+}*/
 
 
 function mousePressed(event) {
